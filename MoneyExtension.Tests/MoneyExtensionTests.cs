ebde91135dd3a09fa031bf44dc804c15e5ffcd34
Solange Arasaki <sol.arasaki77@gmail.com>
2023-07-26 23:30:11 -0300

    Finalizado a primeira versão

---
+++ MoneyExtension.Tests/MoneyExtensionTests.cs
@@ -0,0 +1,15 @@
+namespace MoneyExtension.Tests;
+
+[TestClass]
+public class MoneyExtensionTests
+{
+    [TestMethod]
+    public void ShouldConvertDecimalToInt()
+    {
+        decimal valor = 279.98M;// M = decimal 
+        var cents = valor.ToCents();
+
+        Assert.AreEqual(27998, cents);
+       
+    }
+}
